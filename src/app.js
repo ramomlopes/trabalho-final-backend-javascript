f2daba5c8a13cc606c310decb44e7c946c3af0a6
ramomlopes <ramoncezar531@gmail.com>
2026-03-10 22:26:42 -0300

    config: refatoração dos arquivos de configuração do servidor adicionando oberservador dotenv para acesso a variaveis de ambiente

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes.js');
+
+const chamadoService = require('./services/chamadoService.js');
+const logMiddleware = require('./middlewares/logMiddleware.js');
+const LoggerObserver = require('./observers/LoggerObserver.js');
+
+const chamadoRoutes = require('./routes/chamadoRoutes.js');
+const loginRoutes = require('./routes/loginRoutes.js');
+const indicadoresRoutes = require('./routes/indicadoresRoutes.js');
+const relatorioRoutes = require('./routes/relatorioRoutes.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(logMiddleware);
 
-app.use(userRoutes);
+chamadoService.addObservador(new LoggerObserver());
+
+app.use(chamadoRoutes);
+app.use(loginRoutes);
+app.use(indicadoresRoutes);
+app.use(relatorioRoutes);
 
 app.use((req, res) => {
-    return res.status(404).json({ erro: 'PAGE NOT FOUND' });
+    return res.status(404).json({ erro: 'PAGE NOT FOUND: Página não encontrada' });
 });
 
 module.exports = app;
